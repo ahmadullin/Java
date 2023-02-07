ab796d7ca30c9d30c75b99f39af4f7e4ed515099
Kirill <kirillahmadullin@gmail.com>
2023-02-08 02:52:03 +0500

    task_4

--- Solving_task_2/task_4.java
+++ Solving_task_2/task_4.java
@@ -12,22 +12,23 @@ public class task_4 {
         int a = in.nextInt();
         System.out.print("Введите 2-ое число: ");
         int b = in.nextInt();
+        in.close();
         
         StringBuilder sum = new StringBuilder();
-        sum.append(a + " + " + b + " = " + (a + b));
+        sum.append(a).append(" + ").append(b).append(" = ").append(a + b);
         System.out.println(sum);
 
         StringBuilder diff = new StringBuilder();
-        diff.append(a + " - " + b + " = " + (a - b));
+        diff.append(a).append(" - ").append(b).append(" = ").append(a - b);
         System.out.println(diff);
 
         StringBuilder mult = new StringBuilder();
-        mult.append(a + " * " + b + " = " + (a * b));
+        mult.append(a).append(" * ").append(b).append(" = ").append(a * b);
         System.out.println(mult);
 
         StringBuilder div = new StringBuilder();
         String divformat = String.format("%.2f", (float)a / b);
-        div.append(a + " / " + b + " = " + divformat);
+        div.append(a).append(" / ").append(b).append(" = ").append(divformat);
         System.out.print(div);
     }
 }
